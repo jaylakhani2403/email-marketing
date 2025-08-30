1014f5bc799a0c75461c87daeb5110b7da4087fe
LAKHANI JAY NARASHIBHAI <147643147+jaylakhani2403@users.noreply.github.com>
2025-08-30 13:40:39 +0530

    Update constant.js

--- Frontend/src/lib/constant.js
+++ Frontend/src/lib/constant.js
@@ -1,2 +1,2 @@
-export const HOME_API = "http://localhost:8000"
-// export const HOME_API = "https://email-marketing-backend-0fhs.onrender.com"
+// export const HOME_API = "http://localhost:8000"
+export const HOME_API = "https://email-marketing-backend-0fhs.onrender.com"
